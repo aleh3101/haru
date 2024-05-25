a6e638bd2475d970cb4acbf8cfccd745d6384a36
aleh3101 <160163310+aleh3101@users.noreply.github.com>
2024-05-25 18:33:03 -0500

    Create anx.js

---
+++ anx.js
@@ -0,0 +1,548 @@
+(function () {
+  'use strict';
+
+  // Delete the class 'preload' from the body when the page is loaded
+  window.addEventListener('DOMContentLoaded', event => {
+    document.body.classList.remove('preload');
+  });
+
+  // Load a script from a URL and return a promise
+  // @param {string} src
+  // @returns {Promise}
+  const loadScript = src => new Promise((resolve, reject) => {
+    const $script = document.createElement('script');
+    $script.src = src;
+    $script.onload = resolve;
+    $script.onerror = reject;
+    document.body.appendChild($script);
+  });
+
+  // Load a stylesheet from a URL and return a promise
+  // @param {string} href
+  // @returns {Promise}
+  const loadStyle = href => new Promise((resolve, reject) => {
+    const $link = document.createElement('link');
+    $link.href = href;
+    $link.rel = 'stylesheet';
+    $link.onload = resolve;
+    $link.onerror = reject;
+    document.head.appendChild($link);
+  });
+
+  // Decode HTML entities
+  // @param {string} string
+  // @returns {string}
+  const decodeHtml = string => {
+    return string.replace(/\\x(\w{2})/g, (_, hex) => String.fromCharCode(parseInt(hex, 16)));
+  };
+
+  // Check if the element is in the viewport
+  // @param {Object} element - The element object
+  // @param {Function} fn - The function to execute
+  // @param {Object} options - The options object
+  function isObserver(element, fn, options) {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          fn();
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {
+      ...options
+    });
+    observer.observe(element);
+  }
+
+  // Get the attributes of an element
+  // @param {Object} attributes - The attributes object
+  // @returns {Object} - The attributes object
+  function getAttrs(attributes) {
+    return attributes.reduce((acc, attribute) => {
+      acc[attribute.nodeName] = attribute.nodeValue;
+      return acc;
+    }, {});
+  }
+
+  // Convert an object of attributes to a string
+  // @param {Object} attrs - The attributes object
+  // @returns {string} - The attributes string
+  function attrsToString(attrs) {
+    return Object.keys(attrs).map(key => `${key}="${attrs[key]}"`).join(' ');
+  }
+
+  // Normalize the data
+  // @param {string} value - The value to normalize
+  // @return {any}
+  function normalizeData(value) {
+    if (value === 'true') {
+      return true;
+    }
+    if (value === 'false') {
+      return false;
+    }
+    if (Number(value).toString() === value) {
+      return Number(value);
+    }
+    if (value === 'null' || value === '') {
+      return null;
+    }
+    return value;
+  }
+
+  // Get the data attributes from the element
+  // @param {Object} dataset - The dataset object
+  // @return {Object}
+  function getDataAttrs(_ref) {
+    let {
+      dataset = {}
+    } = _ref;
+    const attributes = {};
+    Object.keys(dataset).forEach(key => {
+      attributes[key] = normalizeData(dataset[key]);
+    });
+    return attributes;
+  }
+
+  // Shuffles the array
+  // @param {Array} array - The array to shuffle
+  // @return {Array}
+  function shuffle(array) {
+    const length = array ? array.length : 0;
+    if (!length) {
+      return [];
+    }
+    const result = [...array];
+    for (let index = 0; index < length; index++) {
+      const rand = index + Math.floor(Math.random() * (length - index));
+      [result[index], result[rand]] = [result[rand], result[index]];
+    }
+    return result;
+  }
+
+  // Check if the url is a youtube url
+  // @param {string} url - The url to check
+  // @return {boolean}
+  function isYoutubeUrl(url) {
+    if (url == null || typeof url !== 'string') return false;
+    return url.includes('img.youtube.com');
+  }
+
+  // Check if the url is new blogger url
+  // @param {string} url - The url to check
+  // @return {boolean}
+  function isNewBloggerUrl(url) {
+    if (url == null || typeof url !== 'string') return false;
+    return url.includes('googleusercontent.com/img/a/');
+  }
+
+  // Change the size of the image hosted in Google Servers
+  // @param {string} imgUrl - The url of the image
+  // @param {string} size - https://zkreations.com/image-params
+  // @return {string}
+  function resizeImage(imgUrl, size) {
+    const REG_EXP = /s\d{2}(-w\d{3}-h\d{3})?(?:-c(?:-n)?)/;
+    if (isNewBloggerUrl(imgUrl)) {
+      return imgUrl.replace(/=(.*)$/, `=${size}`);
+    }
+    if (isYoutubeUrl(imgUrl)) {
+      return imgUrl.replace('default', size);
+    }
+    return imgUrl.replace(REG_EXP, size);
+  }
+
+  // Check if the current date exceeded the expiration date
+  // @param {string} key - The key of the localStorage
+  // @return {boolean}
+  const isExpired = key => {
+    const date = localStorage.getItem(key);
+    if (date) {
+      const now = new Date();
+      const diff = now - new Date(date);
+      const days = diff / 1000 / 60 / 60 / 24;
+      return days > 7;
+    }
+    return true;
+  };
+
+  const REG_EXP = /cookieOptions\.(\w+)\) \|\| '(.+)'/g;
+  const cookieJs = '/js/cookienotice.js';
+  const textarea = document.getElementById('bjs');
+  const loadCookieScript = () => {
+    const textareaContent = textarea.value;
+    const cookieOptions = window.cookieOptions || {};
+    const Default = {};
+    textareaContent.replace(REG_EXP, (_, key, value) => {
+      Default[key] = decodeHtml(value);
+    });
+    if (textareaContent.includes(cookieJs)) {
+      loadScript(new URL(cookieJs, window.location.origin).href).then(() => {
+        const {
+          msg = Default.msg,
+          close = Default.close,
+          learn = Default.learn,
+          link = Default.link
+        } = cookieOptions;
+        if (typeof window.cookieChoices?.showCookieConsentBar === 'function') {
+          // eslint-disable-next-line no-undef
+          cookieChoices.showCookieConsentBar(msg, close, learn, link);
+        }
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+  };
+  if (textarea) {
+    loadCookieScript();
+    textarea.remove();
+  }
+
+  const COMMENT_FORM = document.getElementById('comment-form');
+  const FORM_SCRIPT = document.getElementById('form-script');
+  const FORM_RESTORE = document.getElementById('form-restore');
+  const REPLY_BUTTONS = document.querySelectorAll('[data-parent-id]');
+  const ACTIVE_CLASS$2 = 'is-active';
+  const REPLYING_CLASS = 'is-replying';
+  const HAS_REPLY_FORM_CLASS = 'has-reply-form';
+  const rootMargin = '200px';
+  function loadDisqus(container) {
+    const {
+      shortname,
+      postUrl,
+      postId
+    } = container.dataset;
+    window.disqus_config = function () {
+      this.page.url = postUrl;
+      this.page.identifier = postId;
+    };
+    const script = document.createElement('script');
+    script.src = `https://${shortname}.disqus.com/embed.js`;
+    script.setAttribute('data-timestamp', +new Date());
+    document.head.appendChild(script);
+  }
+  function initDisqus() {
+    const DISQUS = document.getElementById('disqus_thread');
+    const DISQUS_BUTTON = document.getElementById('disqus_btn');
+    const DISQUS_COMMENT_COUNT = document.querySelector('.disqus-comment-count');
+    if (DISQUS_COMMENT_COUNT) {
+      const {
+        shortname
+      } = DISQUS_COMMENT_COUNT.dataset;
+      loadScript(`https://${shortname}.disqus.com/count.js`);
+    }
+    if (!DISQUS) return;
+    if (DISQUS_BUTTON) {
+      DISQUS_BUTTON.onclick = () => {
+        loadDisqus(DISQUS);
+        DISQUS_BUTTON.remove();
+      };
+    } else {
+      isObserver(DISQUS, () => {
+        loadDisqus(DISQUS);
+      }, {
+        rootMargin
+      });
+    }
+  }
+  function loadRelayScript(textarea) {
+    if (!textarea) return;
+    const src = textarea.value.replace(/<script.*?src='(.*?)'.*?><\/script>/, '$1');
+    textarea.remove();
+    loadScript(src)
+    // eslint-disable-next-line no-undef
+    .then(() => BLOG_CMT_createIframe('https://www.blogger.com/rpc_relay.html')).catch(err => console.error(err));
+  }
+  function createIframe(template, newSrc) {
+    if (!template) return;
+    const regex = /<iframe[^>]*\s+src="([^"]*)"/i;
+    const match = template.match(regex);
+    const originalSrc = match[1];
+    const form = newSrc ? template.replace(originalSrc, newSrc) : template;
+    return {
+      originalSrc,
+      form
+    };
+  }
+  const replyComments = buttons => {
+    // Load relay script
+    isObserver(COMMENT_FORM, () => {
+      loadRelayScript(FORM_SCRIPT);
+    }, {
+      rootMargin
+    });
+    if (!buttons) {
+      return;
+    }
+    const template = COMMENT_FORM.innerHTML;
+    const {
+      originalSrc,
+      form: originalForm
+    } = createIframe(template);
+    const replyContainer = document.createElement('div');
+    replyContainer.id = 'reply-form';
+    let currentActiveButton;
+    buttons.forEach(button => {
+      button.onclick = () => {
+        const parent = button.dataset.parentId;
+        const container = document.querySelector(`#c${parent} .comments-replies`);
+        const currentReply = document.getElementById('reply-form');
+        if (currentActiveButton === button) {
+          return;
+        }
+        if (currentReply) {
+          currentReply.parentElement.classList.remove(HAS_REPLY_FORM_CLASS);
+          currentReply.remove();
+        } else {
+          COMMENT_FORM.innerHTML = '';
+          FORM_RESTORE.classList.add(REPLYING_CLASS);
+        }
+        if (currentActiveButton) {
+          currentActiveButton.classList.remove(ACTIVE_CLASS$2);
+        }
+        const newSrc = `${originalSrc}&parentID=${parent}`;
+        const {
+          form: newForm
+        } = createIframe(template, newSrc);
+        button.classList.add(ACTIVE_CLASS$2);
+        currentActiveButton = button;
+        replyContainer.innerHTML = newForm;
+        container.classList.add(HAS_REPLY_FORM_CLASS);
+        container.insertAdjacentElement('afterbegin', replyContainer);
+      };
+    });
+    if (!FORM_RESTORE) {
+      return;
+    }
+    FORM_RESTORE.onclick = () => {
+      if (currentActiveButton) {
+        const currentReply = document.getElementById('reply-form');
+        FORM_RESTORE.classList.remove(REPLYING_CLASS);
+        COMMENT_FORM.innerHTML = originalForm;
+        currentActiveButton.classList.remove(ACTIVE_CLASS$2);
+        currentActiveButton = null;
+        currentReply.parentElement.classList.remove(HAS_REPLY_FORM_CLASS);
+        currentReply.remove();
+      }
+    };
+  };
+  if (COMMENT_FORM) {
+    replyComments(REPLY_BUTTONS);
+  }
+  initDisqus();
+
+  const navElements = document.querySelectorAll('.nav');
+  const ACTIVE_CLASS$1 = 'is-open';
+  let isEventListenerActive = false;
+
+  // Remove the underscore from the subnav links
+  // @param {NodeList} links - The link elements
+  function handleSubnavLinks(links) {
+    links.forEach(link => {
+      link.textContent = link.textContent.trim().replace(/^_+/, '');
+    });
+  }
+  navElements.forEach(nav => {
+    const subnavLinks = nav.querySelectorAll('.nav-subnav .nav-link');
+    const subnavToggles = nav.querySelectorAll('.has-subnav .nav-item-toggle');
+    handleSubnavLinks(subnavLinks);
+    subnavToggles.forEach(toggle => {
+      toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle(ACTIVE_CLASS$1);
+        const isOpen = nav.querySelector(`.${ACTIVE_CLASS$1}`);
+        if (isOpen && !isEventListenerActive) {
+          document.addEventListener('click', clickOutside);
+          isEventListenerActive = true;
+        }
+      });
+    });
+    function clickOutside(event) {
+      if (!nav.contains(event.target)) {
+        nav.querySelectorAll('.has-subnav').forEach(item => {
+          item.classList.remove(ACTIVE_CLASS$1);
+        });
+        document.removeEventListener('click', clickOutside);
+        isEventListenerActive = false;
+      }
+    }
+  });
+
+  const buttons = document.querySelectorAll('[data-outside]');
+  const ACTIVE_CLASS = 'is-active';
+
+  // Toggle the target element
+  // @param {HTMLElement} button - The button element
+  function outsideClick(button) {
+    if (!button) return;
+    const target = document.getElementById(button.dataset.outside);
+    const affected = document.getElementById(button.dataset.affected);
+    if (!target) return;
+    function toggleClasses() {
+      button.classList.toggle(ACTIVE_CLASS);
+      target.classList.toggle(ACTIVE_CLASS);
+      if (affected) affected.classList.toggle(ACTIVE_CLASS);
+      if (button.classList.contains(ACTIVE_CLASS)) {
+        document.addEventListener('click', clickOutside);
+        return;
+      }
+      document.removeEventListener('click', clickOutside);
+    }
+    button.addEventListener('click', toggleClasses);
+    function clickOutside(event) {
+      if (!target.contains(event.target) && !button.contains(event.target)) {
+        toggleClasses();
+        document.removeEventListener('click', clickOutside);
+      }
+    }
+    const closeButton = target.querySelector('[data-close]');
+    if (closeButton) {
+      closeButton.addEventListener('click', toggleClasses);
+    }
+  }
+  buttons.forEach(button => {
+    outsideClick(button);
+  });
+
+  const POST_BODY_CLASS = '.post-body';
+
+  const TOGGLE = document.querySelector('.js-scheme');
+  const isPreview = document.body.classList.contains('is-preview');
+  const STORE_KEY = 'current_theme';
+
+  // Modo Oscuro
+  const toggleMode = () => {
+    const html = document.documentElement;
+    const meta = document.querySelector('meta[name="color-scheme"]');
+    const mode = !isPreview && localStorage.getItem(STORE_KEY) || html.dataset.theme || 'light';
+    const newMode = mode === 'dark' ? 'light' : 'dark';
+    html.dataset.theme = newMode;
+    meta.content = newMode;
+    if (!isPreview) {
+      localStorage.setItem(STORE_KEY, newMode);
+    }
+  };
+  TOGGLE && TOGGLE.addEventListener('click', toggleMode);
+
+  const BLOG_ID = document.querySelector('meta[name="home-blog-admin"]');
+  const CSS = 'https://www.blogger.com/dyn-css/authorization.css?targetBlogID=';
+  if (BLOG_ID) {
+    window.onload = () => {
+      const blogId = BLOG_ID.getAttribute('content');
+      loadStyle(`${CSS}${blogId}`);
+    };
+  }
+
+  const container = document.getElementById('related-posts');
+  const Default = {
+    homepage: window.location.origin,
+    id: 0,
+    image: '',
+    maxResults: 3,
+    shuffleLevel: 5,
+    imageSize: 'w300-h225-p-k-no-nu-rw-l80-e30',
+    ytThumbnail: 'mqdefault',
+    rootMargin: '200px'
+  };
+  function getPostId(entry) {
+    return entry.id.$t.split('.post-')[1];
+  }
+  function createQueryByTags(labels) {
+    return JSON.parse(labels).map(label => `label:"${label}"`).join('|');
+  }
+  function getAlternateLink(links) {
+    for (const link of links) {
+      if (link.rel === 'alternate') {
+        return link.href;
+      }
+    }
+  }
+  function getFirstImage(html) {
+    const temporal = document.createElement('div');
+    temporal.innerHTML = html;
+    const img = temporal.querySelector('img');
+    return img ? img.src : '';
+  }
+  function createEntry(entry) {
+    const $item = document.createElement('a');
+    const $title = document.createElement('div');
+    const $imgContainer = document.createElement('div');
+    $item.className = 'related-item flex column g-1';
+    $imgContainer.className = 'related-image ratio ratio-4:3 color-text flex items-center justify-center i-m';
+    $title.classList.add('related-title');
+    if (entry.image) {
+      const $img = document.createElement('img');
+      $img.className = 'related-image-src image-fit';
+      $img.src = entry.image;
+      $img.alt = 'thumbnail';
+      $item.classList.add('has-image');
+      $imgContainer.appendChild($img);
+    } else {
+      $imgContainer.innerHTML = `
+      <svg class="i i-quote-right" viewBox="0 0 24 24">
+        <path d="M10 11H5V6h5v7a4 5 0 0 1-4 5m13-7h-5V6h5v7a4 5 0 0 1-4 5"></path>
+      </svg>
+    `;
+    }
+    $item.href = entry.url;
+    $title.textContent = entry.title;
+    $item.appendChild($imgContainer);
+    $item.appendChild($title);
+    return $item;
+  }
+  function sanitizeEntry(entry, options) {
+    const content = entry.content?.$t || entry.summary.$t;
+    const title = entry.title.$t;
+    const url = getAlternateLink(entry.link);
+    let image = entry.media$thumbnail?.url || getFirstImage(content) || options.image;
+    image = image !== '' ? resizeImage(image, isYoutubeUrl(image) ? options.ytThumbnail : options.imageSize) : null;
+    return {
+      image,
+      title,
+      url
+    };
+  }
+  function relatedPosts(data, options) {
+    if (data.feed.entry) {
+      let entries = data.feed.entry;
+      if (options.shuffleLevel) {
+        entries = entries.filter(entry => getPostId(entry) !== options.id);
+        entries = shuffle(entries).slice(0, options.maxResults);
+      }
+      const processedData = entries.map(entry => sanitizeEntry(entry, options));
+      const $fragment = document.createDocumentFragment();
+      if (processedData.length === 0) {
+        container.classList.add('related-is-empty');
+        return;
+      }
+      processedData.forEach(entry => {
+        const $entry = createEntry(entry);
+        $fragment.appendChild($entry);
+      });
+      container.innerHTML = '';
+      container.classList.add('related-is-loaded');
+      container.appendChild($fragment);
+    }
+  }
+  function createFeedScript(options) {
+    const $script = document.createElement('script');
+    let feedUrl = `${options.homepage}/feeds/posts/summary?alt=json-in-script&callback=relatedPosts`;
+    if (options.tags) {
+      feedUrl += `&q=${createQueryByTags(options.tags)}`;
+    }
+    feedUrl += `&max-results=${options.maxResults + options.shuffleLevel}`;
+    $script.src = feedUrl;
+    document.body.appendChild($script).parentNode.removeChild($script);
+  }
+  if (container) {
+    const options = {
+      ...Default,
+      ...getDataAttrs(container)
+    };
+    window.relatedPosts = data => relatedPosts(data, options);
+    isObserver(container, () => {
+      createFeedScript(options);
+    }, {
+      rootMargin: options.rootMargin
+    });
+  }
+
+})();
